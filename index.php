a0fbde5364bfbc69c3d4b799ff3aeee2fd427ce8
Sgushonka <Sgushonka>
2016-05-23 09:45:21 +0300

    podcliucenie

---
+++ index.php
@@ -0,0 +1,218 @@
+﻿
+<html>
+<head>
+<title>Landing Page</title>
+<meta http-equiv="Content-Type" content="text/html"; charset="utf-8">
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,700italic,400,700,800' rel='stylesheet' type='text/css'>
+<link href="Dstyle.css" rel="stylesheet" type="text/css">
+<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+ <style type="text/css">
+	  html, body, #map-canvas { height: 100%; margin: 0; padding: 0;}
+	  #menu{
+		display: none;
+	  }
+	</style>
+	
+	
+
+</head>
+<body>
+<section id="s1">
+	<div id="main">
+		<div id="header">
+			<div id="logo">
+				<img src>
+			</div>
+
+			<div id="connecting">
+				<p>Stay connected</p>
+				<div id="img1">
+					
+					</div>
+
+				</div>
+				<div id="img2">
+					
+					</div>
+				</div>
+				<div id="img3">
+					
+					</div>
+				</div>
+			</div>
+		</div>
+			
+
+		<div id="land_discript">
+			<div id="big_discript">
+				
+			</div>
+			<div id="small_discript">
+				
+			</div>
+
+			<div id="contact_button">
+				<div id="contact_us_b">
+					<p>Add your comment</p>
+				</div>
+				<div id="note">
+				</div>
+				
+			</div>
+		</div>
+		
+	</div>
+
+</section>
+
+<section id="s2">
+	<div id="f_block_wrap">
+
+
+
+
+		<div id="first_block">
+			<div class="one_element">
+				
+			</div>
+			<div class="one_element">
+				
+			</div>
+			<div class="one_element">
+				
+			</div>
+			<div class="one_element">
+				
+			</div>
+		</div>
+	</div>
+
+
+
+	<div id="amazing_think">
+		
+		
+	</div>
+
+
+	<div id="counters_wrap">
+		<div id="counters">
+			
+			</div>
+			<div class="counter">
+				
+			</div>
+			<div class="counter">
+				
+			</div>
+			<div class="counter">
+				
+			</div>
+		</div>
+	</div>
+
+
+	
+	<div id="serv_wrap">
+		<div id="services">
+			
+
+			<div id="plans">
+				<div id="plan1">
+					<div id="plan_head1">
+						
+					</div>
+					<div id="plan_main1">
+						
+					</div>
+					<div id="plan_foot1">
+						<div id="plan_foot_button1">
+							<a href=""><p>Subscribe</p></a>
+						</div>
+					</div>
+				</div>
+
+
+
+				<div id="plan2">
+					<div id="plan_head2">
+						
+					</div>
+					<div id="plan_main2">
+						
+					</div>
+					<div id="plan_foot2">
+						<div id="plan_foot_button2">
+							<a href=""><p>Subscribe</p></a>
+						</div>
+					</div>
+				</div>
+
+
+
+
+				<div id="plan3">
+					<div id="plan_head3">
+						
+					</div>
+					<div id="plan_main3">
+										
+					</div>
+					<div id="plan_foot3">
+						<div id="plan_foot_button3">
+							<a href=""><p>Subscribe</p></a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div id="logos_wrap">
+		<div id="logos">
+		</div>
+	</div>
+
+	<div id="clietns_say_wrap">
+		<div id="clients_say">
+
+		</div>
+	</div>
+	
+
+</section>
+
+<section id="s3">
+
+	<div id="main3">
+		<div id="map">
+			
+		</div>
+		
+		<div id="form">
+			
+			<div id="form_but">
+				
+				
+			</div>
+		</form>
+		</div>
+		<div id="comments"  style="height:400px; color:white;">
+		
+		</div>
+	</div>
+	
+
+</section>
+
+<section id="s4">
+
+
+	<div id="main4">
+		
+		
+		</div>
+</section>
+
+</body>
+</html>
